f8849872d98a0eaf683b46db0d1b7a176b496969
Tom Savage <tsavage@gmail.com>
2020-06-24 02:11:39 -0400

    index model

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
+const db = require("./models");
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
